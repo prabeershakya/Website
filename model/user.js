09b7bb0d9f8a8908d8f03ab60ea2757f0650b10c
Prabeer Shakya <prabeershakya123@gmail.com>
2025-05-30 10:29:42 +0545

    Connected DB

---
+++ model/user.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/database");
+
+const User = sequelize.define(
+    "User",
+    {
+        id: { 
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+            isEmail: true,
+            },
+        },
+        // Image: DataTypes.STRING,
+        // allowNull:false,
+    password: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        },
+    },
+    {
+        tableName: "users",
+        timestamps: true,
+    }
+);
+
+module.exports = {User}
--- userController/userController.js
+++ userController/userController.js
@@ -1,15 +1,24 @@
-const createUsers =(req,res) => {
-    res.send('user created')
-}
+const { User } = require("../model/user");
+
+const createUsers = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    await User.create({ username, email, password });
+    res.status(201).json({ success: true, message: "User created !!" });
+  } catch (error) {
+    res.status(400).json({ error: error.message }); // optional: send just error message
+  }
+};
+
 
 const loginUser =(req,res) => {
     res.send('user logedin')
 }
 
 const resetUser =(req,res) => {
-    res.send('user logedin')
+    res.send('user reset')
 }
 
 module.exports = {
     createUsers, loginUser, resetUser
-};
+};  
